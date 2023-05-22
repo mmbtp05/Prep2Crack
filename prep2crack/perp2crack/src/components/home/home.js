b830e5fb92e12ed5b25fc7610cbde2c69d403b76
mukund <maheshwari31mukund@gmail.com>
2023-05-22 17:08:51 +0530

    first model to show

--- prep2crack/perp2crack/src/components/home/home.js
+++ prep2crack/perp2crack/src/components/home/home.js
@@ -3,6 +3,11 @@ import * as React from 'react'
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import './homeStyles.css'
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import Stack from '@mui/material/Stack';
 
 
 const Home = () => {
@@ -11,21 +16,95 @@ const Home = () => {
         <>
             <Container maxWidth="xl" style={{ height: '600px', backgroundColor: 'blue' }}>
                 <Grid container>
-                    <Grid item xl={6} lg={6} className="herocontentleft" sx={{ pl: '70px', pt: '100px' }}>
+                    <Grid item xl={5} lg={5} className="herocontentleft" sx={{ pl: '70px', pt: '150px' }}>
                         <Typography className='upperhead'>
                             One Step Solution For
                         </Typography>
-                        <Typography variant='h1' className='mainhead'>
+                        <Typography variant='h2' className='mainhead'>
                             Study Abroad Aspirants
                         </Typography>
                         <Typography className="lowerhead">
                             Helping students achieve their study abroad dream.
                         </Typography>
+                        <Button variant="contained" className='button' sx={{ mt: '50px' }} disableElevation>Enroll Now</Button>
                     </Grid>
-                    <Grid item xl={6} lg={6}>
+                    <Grid item xl={7} lg={7}>
 
                     </Grid>
                 </Grid>
+            </Container>
+            <Container maxWidth="xl">
+                <Grid container sx={{ mt: '50px', mb: '50px' }}>
+                    {service.map((s, i) => (
+                        <Grid item xl={4} lg={4} className="cardcontainer">
+                            <Card sx={{ maxWidth: 345 }} className="card">
+                                <CardContent>
+                                    <Typography className="cardhead">
+                                        {s.head}
+                                    </Typography>
+                                    <Typography className="cardsubhead">
+                                        {s.subhead}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {s.content}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+            <Container maxWidth="xl">
+                <Grid container sx={{m: '20px'}}> 
+                    <Grid item lg={6} xl={6}>
+
+                    </Grid>
+                    <Grid item lg={6} xl={6} sx={{ pl: '20px', pr: '100px' }}>
+                        <Typography className="whyhead">
+                            Why Prep2Crack?
+                        </Typography>
+                        <Typography className="whysubhead">
+                            Personalized attention, Result driven approach, Economic
+                        </Typography>
+                        <Typography>
+                            The align-content property specifies how flex lines are distributed along the cross axis in a flexbox container.
+
+                            In flexbox layout, the main axis is in the flex-direction (default is 'row', horizontal), and the cross axis is perpendicular to the main axis (default is 'column', vertical).
+
+                            Tip: Use the justify-content property to align the items on the main axis (horizontally).
+
+                            Note: The align-content property can also be used on a grid container to align grid items in the block direction. For pages in English, block direction is downward and inline direction is left to right.
+                        </Typography>
+                        <Card className="whycard">
+                            <CardContent>
+                                <Stack direction="row" spacing={1}>
+                                    <FiberManualRecordIcon />
+                                    <Typography>
+                                        Customized Tutoring
+                                    </Typography>
+                                </Stack>
+                                <Stack direction="row" spacing={1}>
+                                    <FiberManualRecordIcon />
+                                    <Typography>
+                                        100% Assistance of Application Process
+                                    </Typography>
+                                </Stack>
+                                <Stack direction="row" spacing={1}>
+                                    <FiberManualRecordIcon />
+                                    <Typography>
+                                        Guidance for Education Loan
+                                    </Typography>
+                                </Stack>
+                            </CardContent>
+                        </Card>
+                        <Button variant="contained" className='whybutton'   >
+                            Talk to our experts
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Container>
+            <Container>
+                    
             </Container>
         </>
 
@@ -33,3 +112,22 @@ const Home = () => {
 }
 
 export default Home
+
+const service = [
+    {
+        head: 'Test',
+        subhead: 'Preperation',
+        content: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+    },
+    {
+        head: 'Application',
+        subhead: 'Counselling',
+        content: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+    },
+    {
+        head: 'Career',
+        subhead: 'Guidance',
+        content: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+    },
+]
+
--- prep2crack/perp2crack/src/layout/layout.js
+++ prep2crack/perp2crack/src/layout/layout.js
@@ -3,6 +3,18 @@ import { Outlet } from 'react-router-dom'
 import Container from '@mui/material/Container';
 import { Link } from 'react-router-dom';
 import './stylesLayout.css'
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+import { Typography } from '@mui/material'
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+
+
+
 
 const Layout = () => {
 
@@ -11,7 +23,7 @@ const Layout = () => {
 
 
 
-            <Container maxWidth="xl" style={{backgroundColor: 'blue'}}>
+            <Container maxWidth="xl">
                 <nav class="stroke">
                     <ul>
                         <li>
@@ -39,11 +51,117 @@ const Layout = () => {
                                 contactus
                             </Link>
                         </li>
+                        <li>
+                            <Button variant="contained" className='button' disableElevation>Get a quote</Button>
+                        </li>
                     </ul>
                 </nav>
             </Container>
 
             <Outlet />
+
+            <Container style={{ backgroundColor: '#E2F0FF' }} maxWidth="xl">
+                <Grid container className='footerupper'>
+                    <Grid item lg={6} xl={6}>
+
+                    </Grid>
+                    <Grid item lg={6} xl={6} style={{ display: 'flex', justifyContent: 'right', alignItems: 'center' }}>
+                        <Stack direction="row" spacing={1}>
+                            <FacebookIcon />
+                            <InstagramIcon />
+                            <LinkedInIcon />
+                        </Stack>
+                    </Grid>
+                </Grid>
+                <Divider variant="middle" />
+                <Grid container sx={{ mt: '20px', pb: '20px' }}>
+                    <Grid item lg={3} xl={3}>
+                        <Typography className='footerabtus'>
+                            About Us
+                        </Typography>
+                        <Typography>
+                            London. Michaelmas term lately over, and the Lord Chancellor sitting in Lincoln's Inn Hall. Implacable November weather. As much mud in the streets as if the waters had but newly retired from the face of the earth, and it would not be wonderful to meet a Megalosaurus, forty feet long or so, waddling like an elephantine lizard up Holborn Hill.
+                        </Typography>
+                    </Grid>
+                    <Grid item lg={3} xl={3}>
+                        <Typography className='footerabtus'>
+                            Services
+                        </Typography>
+                        <Stack direction="row" spacing={1}>
+                            <FiberManualRecordIcon />
+                            <Typography>
+                                Test Preperation
+                            </Typography>
+                        </Stack>
+                        <Stack direction="row" spacing={1}>
+                            <FiberManualRecordIcon />
+                            <Typography>
+                                Application Counselling
+                            </Typography>
+                        </Stack>
+                        <Stack direction="row" spacing={1}>
+                            <FiberManualRecordIcon />
+                            <Typography>
+                                Career Guidance
+                            </Typography>
+                        </Stack>
+                    </Grid>
+                    <Grid item lg={3} xl={3}>
+                        <Typography className='footerabtus'>
+                            Quick Links
+                        </Typography>
+                        <Stack direction="row" spacing={1}>
+                            <FiberManualRecordIcon />
+                            <Typography>
+                                Home
+                            </Typography>
+                        </Stack>
+                        <Stack direction="row" spacing={1}>
+                            <FiberManualRecordIcon />
+                            <Typography>
+                               About Us
+                            </Typography>
+                        </Stack>
+                        <Stack direction="row" spacing={1}>
+                            <FiberManualRecordIcon />
+                            <Typography>
+                                Services
+                            </Typography>
+                        </Stack>
+                        <Stack direction="row" spacing={1}>
+                            <FiberManualRecordIcon />
+                            <Typography>
+                                Study Destination
+                            </Typography>
+                        </Stack>
+                        <Stack direction="row" spacing={1}>
+                            <FiberManualRecordIcon />
+                            <Typography>
+                                Contact Us
+                            </Typography>
+                        </Stack>
+                    </Grid>
+                    <Grid item lg={3} xl={3}>
+                        <Typography className='footerabtus'>
+                            Contact info
+                        </Typography>
+                        <Typography>
+                           319-B, Scheme No. 1, Arya Nagar, Alwar
+                        </Typography>
+                        <Typography>
+                           Phone     +91-7742842292
+                        </Typography>
+                        <Typography>
+                           E-mail    preptocrack@gmail.com
+                        </Typography>
+                        <Typography>
+                           website   www.preptocrack.com
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+
+
 
 
 
